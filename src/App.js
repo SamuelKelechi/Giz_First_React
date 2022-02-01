3141b7c703e8e3b92c96d46947619d8248c8fa41
SamuelKelechi <kcfreshie@ymail.com>
2022-02-01 04:49:06 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [add, setAdd] = useState(5)
+
+
+  function Add(){
+    setAdd(add +1)
+  }
+
+  const Minus = ()=> {
+    setAdd(add -1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={Add}>Add</button>
+      <div>{add}</div>
+      <button onClick={Minus}>Minus</button>
     </div>
   );
 }
